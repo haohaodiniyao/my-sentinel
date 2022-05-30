077bd31a1a28953bb7b37e2c69850c67fd84db2d
haohaodiniyao <1270305593@qq.com>
2022-05-30 22:59:22 +0800

    sentinel

--- src/main/java/com/example/mysentinel/config/SentinelAspectConfig.java
+++ src/main/java/com/example/mysentinel/config/SentinelAspectConfig.java
@@ -1,4 +1,17 @@
 package com.example.mysentinel.config;
 
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * sentinel
+ * 注解拦截
+ */
+@Configuration
 public class SentinelAspectConfig {
+    @Bean
+    public SentinelResourceAspect sentinelResourceAspect(){
+        return new SentinelResourceAspect();
+    }
 }
